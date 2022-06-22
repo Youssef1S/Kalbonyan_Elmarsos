f3d7ba8b6e852b6a7dae2ec9071e4fc5f6f77502
Youssef1S <yosifshaban6@gmail.com>
2022-06-22 12:18:09 +0200

    update script file

--- Udemy/12-JavaScript/09-Working-with-Arrays/Challenge09-04/script.js
+++ Udemy/12-JavaScript/09-Working-with-Arrays/Challenge09-04/script.js
@@ -18,15 +18,16 @@ HINT 1: Use many different tools to solve these challenges, you can use the summ
 HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
 
 TEST DATA:
+
+GOOD LUCK 😀
+*/
+
 const dogs = [
   { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
   { weight: 8, curFood: 200, owners: ['Matilda'] },
   { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
   { weight: 32, curFood: 340, owners: ['Michael'] }
 ];
-
-GOOD LUCK 😀
-*/
 
 // 1.
 dogs.forEach(dog => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
