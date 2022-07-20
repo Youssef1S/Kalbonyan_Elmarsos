ea9c96d185b2cb57b544de3de01f526d044c6c7f
Youssef1S <yosifshaban6@gmail.com>
2022-07-20 15:57:35 +0200

    Add a new section

---
+++ Udemy/13-React-Js/05-Time-to-practice/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (userName, userAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        {
+          name: userName,
+          age: userAge,
+          id: Math.round(Math.random() * 20000).toString,
+        },
+        ...prevUsersList,
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
