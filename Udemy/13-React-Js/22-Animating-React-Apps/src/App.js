7c56d383386a38209616e632531eaaadb65c32e8
Youssef1S <yosifshaban6@gmail.com>
2022-08-16 05:14:44 +0200

    Add a new section

--- Udemy/13-React-Js/22-Animating-React-Apps/src/App.js
+++ Udemy/13-React-Js/22-Animating-React-Apps/src/App.js
@@ -9,7 +9,7 @@ import List from "./components/List/List";
 class App extends Component {
   state = {
     modalIsOpen: false,
-    showBlock: false,
+    showBlock: false
   };
 
   showModal = () => {
@@ -27,28 +27,24 @@ class App extends Component {
         <button
           className="Button"
           onClick={() =>
-            this.setState((prevStaste) => ({
-              showBlock: !prevStaste.showBlock,
-            }))
-          }
+            this.setState(prevState => ({ showBlock: !prevState.showBlock }))}
         >
           Toggle
         </button>
         <br />
-
         <Transition
           in={this.state.showBlock}
           timeout={1000}
           mountOnEnter
           unmountOnExit
-          onEnter={() => console.log("onEnter")}
-          onEntering={() => console.log("onEntering")}
-          onEntered={() => console.log("onEntered")}
-          onExit={() => console.log("onExit")}
-          onExiting={() => console.log("onExiting")}
-          onExited={() => console.log("onExited")}
+          onEnter={() => console.log('onEnter')}
+          onEntering={() => console.log('onEntering')}
+          onEntered={() => console.log('onEntered')}
+          onExit={() => console.log('onExit')}
+          onExiting={() => console.log('onExiting')}
+          onExited={() => console.log('onExited')}
         >
-          {(state) => (
+          {state => (
             <div
               style={{
                 backgroundColor: "red",
@@ -56,13 +52,12 @@ class App extends Component {
                 height: 100,
                 margin: "auto",
                 transition: "opacity 1s ease-out",
-                opacity: state === "exiting" ? 0 : 1,
+                opacity: state === "exiting" ? 0 : 1
               }}
-            ></div>
+            />
           )}
         </Transition>
         <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-
         {this.state.modalIsOpen ? <Backdrop show /> : null}
         <button className="Button" onClick={this.showModal}>
           Open Modal
--- Udemy/13-React-Js/22-Animating-React-Apps/src/components/List/List.js
+++ Udemy/13-React-Js/22-Animating-React-Apps/src/components/List/List.js
@@ -6,21 +6,21 @@ import "./List.css";
 
 class List extends Component {
   state = {
-    items: [1, 2, 3],
+    items: [1, 2, 3]
   };
 
   addItemHandler = () => {
-    this.setState((prevState) => {
+    this.setState(prevState => {
       return {
-        items: prevState.items.concat(prevState.items.length + 1),
+        items: prevState.items.concat(prevState.items.length + 1)
       };
     });
   };
 
-  removeItemHandler = (selIndex) => {
-    this.setState((prevState) => {
+  removeItemHandler = selIndex => {
+    this.setState(prevState => {
       return {
-        items: prevState.items.filter((item, index) => index !== selIndex),
+        items: prevState.items.filter((item, index) => index !== selIndex)
       };
     });
   };
@@ -28,7 +28,9 @@ class List extends Component {
   render() {
     const listItems = this.state.items.map((item, index) => (
       <CSSTransition key={index} classNames="fade" timeout={300}>
-        <li className="ListItem" onClick={() => this.removeItemHandler(index)}>
+        <li
+          className="ListItem"
+          onClick={() => this.removeItemHandler(index)}>
           {item}
         </li>
       </CSSTransition>
@@ -40,7 +42,7 @@ class List extends Component {
           Add Item
         </button>
         <p>Click Item to Remove.</p>
-        <TransitionGroup component="ul" className="List ">
+        <TransitionGroup component="ul" className="List">
           {listItems}
         </TransitionGroup>
       </div>
