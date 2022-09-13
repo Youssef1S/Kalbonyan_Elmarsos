69d88db3da0695aea4d3d61979d6bd896aafe395
Youssef Shaaban <yosifshaban6@gmail.com>
2022-09-13 13:37:40 +0200

    Test the app

---
+++ Udemy/14-Node-Js/03-Task-manager-app/tests/math.test.js
@@ -0,0 +1,51 @@
+const {
+  calculateTip,
+  celsiusToFahrenheit,
+  fahrenheitToCelsius,
+  add
+} = require("../src/math");
+
+// test('Async test demo', (done) => {
+//     setTimeout(() => {
+//         expect(1).toBe(2)
+//         done()
+//     }, 2000)
+// })
+
+// test("Should add two numbers", (done) => {
+//   add(2, 3).then((sum) => {
+//     expect(sum).toBe(5);
+//     done();
+//   });
+// });
+
+// test("Should add two numbers async/await", async () => {
+//   const sum = await add(10, 22);
+//   expect(sum).toBe(32);
+// });
+
+// test("Should convert from 32F to 0C", () => {
+//   const celsius = fahrenheitToCelsius(32);
+//   expect(celsius).toBe(0);
+// });
+
+// test("Should Convert from 0C to 32F", () => {
+//   const fahrenheit = celsiusToFahrenheit(0);
+//   expect(fahrenheit).toBe(32);
+// });
+
+// test("Should calculate total with tip", () => {
+//   const total = calculateTip(10, 0.3);
+//   expect(total).toBe(13);
+// });
+
+// test("Should calculate total with default tip", () => {
+//   const total = calculateTip(10);
+//   expect(total).toBe(12.5);
+// });
+
+// test("Hello World", () => {});
+
+// test("This should fail", () => {
+//   throw new Error("Failure");
+// });
