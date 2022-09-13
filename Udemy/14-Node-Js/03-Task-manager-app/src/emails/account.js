536f4dfe6ed19321c86c5c7bddcbb3f6d6e9368d
Youssef Shaaban <yosifshaban6@gmail.com>
2022-09-13 13:19:39 +0200

    make it better

--- Udemy/14-Node-Js/03-Task-manager-app/src/emails/account.js
+++ Udemy/14-Node-Js/03-Task-manager-app/src/emails/account.js
@@ -1,28 +1,26 @@
-const sgMail = require("@sendgrid/mail");
-// const sendgridAPIKey =
-//   "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";
+const sgMail = require('@sendgrid/mail')
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeEmail = (email, name) => {
-  sgMail.send({
-    to: email,
-    from: "youssef.shaaban160916@ci.menofia.edu.eg",
-    subject: "Thanks for joining us",
-    text: `Welcome to the app, ${name}. Let me know how you get along with the app.`,
-  });
-};
+    sgMail.send({
+        to: email,
+        from: 'andrew@mead.io',
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    })
+}
 
-const sendCancelEmail = (email, name) => {
-  sgMail.send({
-    to: email,
-    from: "youssef.shaaban160916@ci.menofia.edu.eg",
-    subject: `Sorry to see you go!`,
-    text: `Goodbye, ${name}. I hope to see you back sometime soon.`,
-  });
-};
+const sendCancelationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'andrew@mead.io',
+        subject: 'Sorry to see you go!',
+        text: `Goodbye, ${name}. I hope to see you back sometime soon.`
+    })
+}
 
 module.exports = {
-  sendWelcomeEmail,
-  sendCancelEmail,
-};
+    sendWelcomeEmail,
+    sendCancelationEmail
+}
